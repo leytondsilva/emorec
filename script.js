a239bffa7f63c6c9cae2e29950f1b9eb4f77d30f
leytondsilva <leytondsilva@gmail.com>
2020-06-11 20:12:09 +0530

    ss

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function startVideo() {
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
+  const displaySize = { width: 50%, height: 50vh }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
