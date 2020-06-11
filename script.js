b125d51d8993b6459305c5e853e24c1a1773bb7a
leytondsilva <leytondsilva@gmail.com>
2020-06-11 14:46:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const video = document.getElementById('video')
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
+]).then(startVideo())
 
 function startVideo() {
   navigator.getUserMedia(
